e9527b29d35eb1e83e83a93a17cdc900b2218b23
JohnPetros <joaopedro.nc@outlook.com>
2024-11-16 08:07:15 -0300

    🧱 components: add go back

---
+++ apps/web/src/ui/components/go-back-link/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+  href: String,
+})
+</script>
+
+<template>
+  <a :href="href" class="flex items-center gap-3 text-slate-50">
+    <i class="pi pi-angle-left font-bold text-indigo-500"></i>
+    go back
+  </a>
+</template>
