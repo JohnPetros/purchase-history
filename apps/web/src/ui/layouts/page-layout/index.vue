c45db663a3b310b7c2c6973738d4e259755ae217
JohnPetros <joaopedro.nc@outlook.com>
2024-11-16 08:09:25 -0300

    💄 styles: add background color to body page

--- apps/web/src/ui/layouts/page-layout/index.vue
+++ apps/web/src/ui/layouts/page-layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[#141625] min-h-screen overflow-x-hidden">
+  <div class="min-h-screen overflow-x-hidden">
     <aside
       class="fixed flex flex-col justify-between top-0 left-0 bottom-0 bg-slate-800 rounded-r-3xl"
     >
@@ -10,8 +10,8 @@
       </div>
       <div class="w-full h-px border-t border-slate-600 mt-auto py-12"></div>
     </aside>
-    <div class="mx-auto max-w-xl py-20">
+    <main class="mx-auto max-w-xl py-20">
       <slot></slot>
-    </div>
+    </main>
   </div>
 </template>
