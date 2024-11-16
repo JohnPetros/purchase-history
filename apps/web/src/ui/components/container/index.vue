3f1c79fb80c8a16bd49284e8ad68f862af1897d8
JohnPetros <joaopedro.nc@outlook.com>
2024-11-16 08:06:54 -0300

    🧱 components: add container

---
+++ apps/web/src/ui/components/container/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps({
+  className: String,
+})
+</script>
+
+<template>
+  <div :class="`bg-slate-800 rounded-md p-6 ${className}`">
+    <slot></slot>
+  </div>
+</template>
