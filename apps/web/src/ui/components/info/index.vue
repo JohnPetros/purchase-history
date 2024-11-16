90d1ffc80f4ec2d9ba88a52ebf5c90a3d0a13834
JohnPetros <joaopedro.nc@outlook.com>
2024-11-16 08:08:47 -0300

    🧱 components: add info

---
+++ apps/web/src/ui/components/info/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+  label: String,
+  data: String,
+})
+</script>
+
+<template>
+  <dt class="text-sm text-slate-300">{{ label }}</dt>
+  <dd class="flex flex-col gap-1 text-slate-50 font-bold mt-2">
+    <slot></slot>
+  </dd>
+</template>
