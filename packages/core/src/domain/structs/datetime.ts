eb5221d60507c8f538a9231e9f6b4c409482aace
JohnPetros <joaopedro.nc@outlook.com>
2024-11-17 09:14:01 -0300

    🧩 structs: format datetime value

--- packages/core/src/domain/structs/datetime.ts
+++ packages/core/src/domain/structs/datetime.ts
@@ -1,12 +1,16 @@
-import { NotValidError } from '../../errors'
-
 export class Datetime {
   private constructor(readonly value: Date) {}
 
-  static create(value?: Date, key?: string) {
-    if (value && !(value instanceof Date))
-      throw new NotValidError(`${key} must be datetime`)
-
+  static create(value?: Date) {
     return new Datetime(value ?? new Date())
+  }
+
+  format() {
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    })
+    return formatter.format(new Date(this.value))
   }
 }
