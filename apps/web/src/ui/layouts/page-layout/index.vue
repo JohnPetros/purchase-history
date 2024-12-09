bc4c95f8f3d86758b7f04bcb3d2c61f6dfa64cca
JohnPetros <joaopedro.nc@outlook.com>
2024-12-09 09:32:54 -0300

    💄 styles: adapt navbar to mobile

--- apps/web/src/ui/layouts/page-layout/index.vue
+++ apps/web/src/ui/layouts/page-layout/index.vue
@@ -10,8 +10,35 @@ const { path } = useRoute()
   <Toast />
   <DynamicDialog />
   <div class="min-h-screen overflow-x-hidden">
+    <header class="flex items-center justify-between pr-6 fixed md:hidden top-0 right-0 w-full bg-slate-800 ">
+      <div
+        class="grid place-content-center size-24 rounded-r-3xl bg-violet-500"
+      >
+        <img src="@/assets/images/logo.svg" alt="Purchase History" />
+      </div>
+      <nav>
+        <ol class="flex items-center gap-6">
+          <li>
+            <a href="/" :class="`hover:opacity-85 ${path === '/' ? 'text-slate-50' : 'text-slate-400'}`">
+              Products
+            </a>
+          </li>
+          <li>
+            <a href="/suppliers" :class="`hover:opacity-85 ${path === '/suppliers' ? 'text-slate-50' : 'text-slate-400'}`">
+              Suppliers
+            </a>
+          </li>
+          <li>
+            <a href="/invoices" :class="`hover:opacity-85 ${path === '/invoices' ? 'text-slate-50' : 'text-slate-400'}`">
+              Invoices
+            </a>
+          </li>
+        </ol>
+      </nav>
+    </header>
+
     <aside
-      class="fixed flex flex-col justify-between top-0 left-0 bottom-0 bg-slate-800 rounded-r-3xl"
+      class="fixed hidden md:flex flex-col justify-between top-0 left-0 bottom-0 bg-slate-800 rounded-r-3xl"
     >
       <div
         class="grid place-content-center size-32 rounded-r-3xl bg-violet-500"
@@ -41,7 +68,7 @@ const { path } = useRoute()
 
       <div class="w-full h-px border-t border-slate-600 mt-auto py-12"></div>
     </aside>
-    <main class="mx-auto max-w-xl py-20">
+    <main class="mx-auto max-w-xl px-6 md:px-0 py-32 md:py-24">
       <slot></slot>
     </main>
   </div>
