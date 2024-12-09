bdf6be631792f345c0db4ba2f10b0599a11fc256
JohnPetros <joaopedro.nc@outlook.com>
2024-12-09 10:49:49 -0300

    🧱 templates: add pagination component

---
+++ apps/web/src/ui/components/pagination/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Paginator, { type PageState } from 'primevue/paginator'
+
+import { PAGINATION } from '@purchase-history/core/constants'
+
+type Props = {
+  totalRecordsCount: number
+}
+
+defineProps<Props>()
+const emit = defineEmits(['change'])
+</script>
+
+
+<template>
+  <Paginator 
+    :rows="PAGINATION.itemsPerPage" 
+    :totalRecords="totalRecordsCount" 
+    :page="(state: PageState) => emit('change', state.page)">
+  </Paginator>
+</template>
+
